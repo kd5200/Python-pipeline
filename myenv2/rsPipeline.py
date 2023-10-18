17316324c74bfcfdce9451ee589a4b21b19e11dd
kd5200 <kareemdavis18@yahoo.com>
2023-10-18 12:13:07 -0400

    Connected Postgres to Redshift after changing redshift config

--- myenv2/rsPipeline.py
+++ myenv2/rsPipeline.py
@@ -3,6 +3,9 @@ from psycopg2 import Error
 import boto3
 from env import *
 
+
+#testing the pipeline to connect Postgresql to redshift and pass data between the two systems.
+
 # Establish PostgreSQL Connection
 pg_conn = psycopg2.connect(host=pg_host, port=pg_port, dbname=pg_dbname, user=pg_user, password=pg_password)
 pg_cursor = pg_conn.cursor()
